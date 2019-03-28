2f59ac69b1da94a1dcb3773f38b943773649c04c
Cailie Carlile <cailiecarlile@gmail.com>
2019-03-27 19:42:43 -0500

    comments and initial setup

--- HighSchool/Program.cs
+++ HighSchool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HighSchool
 {
@@ -6,6 +7,18 @@ namespace HighSchool
     {
         public static void Main()
         {
+            //contains a Student class. You must track the student’s first name, last name, grade (1 - 12), 
+            //and current GPA (1-4). Store a list of 10 or more fake students in a list. 
+            //Then, use LINQ to print a list of students with a GPA of 3 or higher that are in the 9th - 12th grade. 
+            //The list should output their first name, last name, grade and GPA. 
+            //It should be sorted by highest grade, then highest GPA, then last name, then first name.
+
+            List<Student> studentList = new List<Student>
+            {
+                
+            };
+
+
             Console.ReadLine();
         }
     }
--- HighSchool/Student.cs
+++ HighSchool/Student.cs
@@ -5,8 +5,24 @@ namespace HighSchool
 {
     public class Student
     {
+        //contains a Student class. You must track the student’s first name, last name, grade (1 - 12), 
+        //and current GPA (1-4). Store a list of 10 or more fake students in a list. 
+        //Then, use LINQ to print a list of students with a GPA of 3 or higher that are in the 9th - 12th grade. 
+        //The list should output their first name, last name, grade and GPA. 
+        //It should be sorted by highest grade, then highest GPA, then last name, then first name.
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int grade { get; set; }
+        public float currentGPA { get; set; }
+
         public static List<Student> StarStudents(List<Student> students)
         {
+            List<Student> starStudents = new List<Student>();
+
+
+
+            return starStudents;
         }
     }
 }
